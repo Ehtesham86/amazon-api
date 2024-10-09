2fb8483c629ce0900b607d40df269d5f0ee6fa2a
Ehtesham86 <buttehtesham86@gmail.com>
2024-10-08 23:04:34 -0700

    11 4

--- scraper/amazonScraper.js
+++ scraper/amazonScraper.js
@@ -1,26 +1,30 @@
 const puppeteer = require('puppeteer-core');
-const chromium = require("@sparticuz/chromium");
- 
+// const chromium = require("@sparticuz/chromium");
+const chromium = require('chrome-aws-lambda');
+
 const scrapeProductData = async (asin) => {
+ 
     // const browser = await puppeteer.launch({
-    //     args: chromium.args,
-    //     defaultViewport: chromium.defaultViewport,
-    //     // C:\Program Files\Google\Chrome
-    //     executablePath: 'C:/Program Files/Google/Chrome/Application/Chrome.exe', // Update the path to the Chrome executable
+    //     // args: chromium.args,
+    //      // executablePath: 'C:/Program Files/Google/Chrome/Application/Chrome.exe',
+    //     // headless: true,
+    //     executablePath: process.platform === 'win32' ? 'C:/Program Files/Google/Chrome/Application/Chrome.exe' : '/usr/bin/google-chrome',
+    //     // args: ['--no-sandbox', '--disable-setuid-sandbox'],
+ 
     //     headless: chromium.headless,
     //     ignoreHTTPSErrors: true,
-    // });
+
+
+    //     args: [...chromium.args, '--no-sandbox', '--disable-setuid-sandbox'],
+    //     defaultViewport: chromium.defaultViewport,
+    //   })
     const browser = await puppeteer.launch({
+        executablePath: await chromium.executablePath,
         args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        // executablePath: 'C:/Program Files/Google/Chrome/Application/Chrome.exe',
-        executablePath:'/usr/bin/google-chrome',
-
-        args: ['--no-sandbox'],
-
         headless: chromium.headless,
         ignoreHTTPSErrors: true,
-    })
+        defaultViewport: chromium.defaultViewport,
+      });
     const page = await browser.newPage();
 
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
